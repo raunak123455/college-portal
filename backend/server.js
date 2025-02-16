2d52cd7279394eb635c60d3fe7ce359b7d510680
raunak123455 <91775838+raunak123455@users.noreply.github.com>
2025-02-16 14:01:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 // CORS configuration
 app.use(
   cors({
-    origin: "https://college-portal-livid.vercel.app/", // Your frontend URL
+    origin: "https://college-portal-livid.vercel.app", // Your frontend URL
     credentials: true, // Allow credentials
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
